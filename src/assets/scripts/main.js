d4969d12c16a96a670d811e24aa42c33fcc088f6
Maria Freixa Torres <mery.blood@gmail.com>
2021-04-18 13:25:24 +0200

    miembros & contacto

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -5,12 +5,14 @@
 
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
+import * as bootstrap from 'bootstrap';
 
 /**
  * Write any other JavaScript below
  */
 
+/*
 +( function() {
   const university = "UOC";
   console.log(`Hello, ${university}!`);
-} )();
+} )();*/
